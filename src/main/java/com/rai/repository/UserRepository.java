ac2894fdaa7ba8ed61b6b86207bac0d9b0b12ca0
Mausam Rai <mausamrai11@gmail.com>
2022-11-10 22:47:07 -0500

    initial commit

---
+++ src/main/java/com/rai/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.rai.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.rai.entity.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
